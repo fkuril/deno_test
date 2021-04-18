70ca3249102a016b7cd7c44d5af66823407a7f22
Fedor Kurilov <721079+fkuril@users.noreply.github.com>
2021-04-18 15:22:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+let count = 0;
 addEventListener("fetch", (event) => {
+  count += 1;
   event.respondWith(
-    new Response(JSON.stringify(Object.keys(window)), {
+    new Response(JSON.stringify({...Object.keys(window), count }), {
       status: 200,
       headers: {
         server: "denosr",
